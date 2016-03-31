f272312561c56cf09ef4daaf2a92206dc65ff503
Vincent Sellier <vsellier@exoplatform.com>
2016-03-31 15:13:21 +0200

    Allow to monitor several instances of elasticsearch

--- manifests/agent/sender/elasticsearch.pp
+++ manifests/agent/sender/elasticsearch.pp
@@ -1,7 +1,11 @@
-class zabbix::agent::sender::elasticsearch ($active=true, $sender_data_file_path = '/tmp/zabbix_elasticsearch_status.data') inherits zabbix::params {
+define zabbix::agent::sender::elasticsearch (
+  $active                 = true, 
+  $sender_data_file_path  = '/tmp/zabbix_elasticsearch_status.data',
+  $port                   = 9200, 
+) {
   ensure_packages('jq')
 
-  file { "${zabbix::params::config_other_scripts_dir}/send_elasticsearch.sh":
+  ensure_resource ( 'file', "${zabbix::params::config_other_scripts_dir}/send_elasticsearch.sh", {
     ensure  => $active ? {
       true    => present,
       default => absent
@@ -11,16 +15,16 @@ class zabbix::agent::sender::elasticsearch ($active=true, $sender_data_file_path
     mode    => '0750',
     content => template('zabbix/v2.x/etc/zabbix/other-scripts/send_elasticsearch.sh.erb'),
     require => File[$zabbix::params::config_other_scripts_dir]
-  } ->
-  cron { 'zabbix_sender_elasticsearch':
+  })
+  cron { "zabbix_sender_elasticsearch_${port}" :
     ensure  => $active ? {
       true    => present,
       default => absent
     },
-    command => "${zabbix::params::config_other_scripts_dir}/send_elasticsearch.sh > /dev/null",
+    command => "${zabbix::params::config_other_scripts_dir}/send_elasticsearch.sh ${port} > /dev/null",
     user    => zabbix,
     target  => zabbix,
     minute  => '*',
     hour    => '*'
-  }
+  }    
 }
